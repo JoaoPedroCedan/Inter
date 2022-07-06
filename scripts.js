6010dfa9e86525d96fbb535531723977bdda141c
Gustavo Garcia <sousagustavogarcia@hotmail.com>
2022-07-06 20:26:19 -0300

    header

--- scripts.js
+++ scripts.js
@@ -7,10 +7,3 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
-
-document.querySelectorAll('.sobre .video-container .controls .control-btn').forEach(btn => {
-    btn.onclick = () => {
-        let src = btn.getAttribute('data-src');
-        document.querySelector('.sobre .video-container .video').src = src;
-    }
-})
