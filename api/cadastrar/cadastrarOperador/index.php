b41587d0787ffedb02c3f797878944452a172b6f
Anthony Melo <anthonyhenrique1@outlook.com>
2022-07-24 12:26:09 -0300

    Cadastrar operador index

---
+++ api/cadastrar/cadastrarOperador/index.php
@@ -0,0 +1,19 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once '../../conectiondb.php';
+
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $senha = $_POST['senha'];
+
+    $insert = $conn->prepare("INSERT INTO operador (nome,cpf, senha, is_active) values ('$nome', '$cpf','$senha', '$status')");
+    if($insert->execute()){
+        echo json_encode('efeutado com sucesso') ;
+};
+
+?>
