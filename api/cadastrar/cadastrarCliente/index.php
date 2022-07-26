14fdf0b6ac0f9266aa525af07b771510fc1a1488
JoaoPedroCedan <joao.pedro@oaktecnologia.com>
2022-07-26 20:50:45 -0300

    a

--- api/cadastrar/cadastrarCliente/index.php
+++ api/cadastrar/cadastrarCliente/index.php
@@ -7,22 +7,30 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 require_once '../../conectiondb.php';
 
-$nome = $_POST['nome'];
-$telefone = $_POST['telefone'];
 $cpf = $_POST['cpf'];
-$cep = $_POST['cep'];
-$logradouro = $_POST['logradouro'];
-$numero = $_POST['numero'];
-$bairro = $_POST['bairro'];
-$cidade = $_POST['cidade'];;
-$estado = $_POST['estado'];
-$status = $_POST['status'];
 
-$insert = $conn->prepare("INSERT INTO cliente 
-(nome,telefone,cpf,bairro,cep,logradouro,numero,cidade,estado,is_active) 
-values 
-('$nome','$telefone', '$cpf', '$cep', '$logradouro', '$numero', '$bairro', '$cidade', '$estado', '$status')");
-if($insert->execute()){
-    echo json_encode('efeutado com sucesso') ;
-};
+$select = $conn->query("SELECT cpf FROM cliente where cpf = '$cpf'");
+$select->execute();
+$valor = $select->fetchAll();
+if (count($valor) > 0) {
+    echo json_encode('existe');
+}else{
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $cep = $_POST['cep'];
+    $logradouro = $_POST['logradouro'];
+    $numero = $_POST['numero'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];;
+    $estado = $_POST['estado'];
+    $status = $_POST['status'];
+
+    $insert = $conn->prepare("INSERT INTO cliente 
+    (nome,telefone,cpf,bairro,cep,logradouro,numero,cidade,estado,is_active) 
+    values 
+    ('$nome','$telefone', '$cpf', '$cep', '$logradouro', '$numero', '$bairro', '$cidade', '$estado', '$status')");
+    if($insert->execute()){
+        echo json_encode('efeutado com sucesso') ;
+    };
+}
 ?>
