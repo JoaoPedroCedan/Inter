bfc808f319e54558a23b2eadc7edac5a0eccdd1c
Anthony Melo <anthonyhenrique1@outlook.com>
2022-08-01 21:56:16 -0300

    Alterando editar

---
+++ script.js
@@ -0,0 +1,122 @@
+//formata de forma generica os campos
+
+function formataCampo(campo, Mascara) {
+    var er = /[^0-9/ (),.-]/;
+    er.lastIndex = 0;
+
+    if (er.test(campo.value)) { ///verifica se é string, caso seja então apaga
+        var texto = $(campo).val();
+        $(campo).val(texto.substring(0, texto.length - 1));
+    }
+    var boleanoMascara;
+    var exp = /\-|\.|\/|\(|\)| /g
+    var campoSoNumeros = campo.value.toString().replace(exp, "");
+    var posicaoCampo = 0;
+    var NovoValorCampo = "";
+    var TamanhoMascara = campoSoNumeros.length;
+    for (var i = 0; i <= TamanhoMascara; i++) {
+        boleanoMascara = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".") ||
+            (Mascara.charAt(i) == "/"))
+        boleanoMascara = boleanoMascara || ((Mascara.charAt(i) == "(") ||
+            (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " "))
+        if (boleanoMascara) {
+            NovoValorCampo += Mascara.charAt(i);
+            TamanhoMascara++;
+        } else {
+            NovoValorCampo += campoSoNumeros.charAt(posicaoCampo);
+            posicaoCampo++;
+        }
+    }
+    campo.value = NovoValorCampo;
+    ////LIMITAR TAMANHO DE CARACTERES NO CAMPO DE ACORDO COM A MASCARA//
+    if (campo.value.length > Mascara.length) {
+        var texto = $(campo).val();
+        $(campo).val(texto.substring(0, texto.length - 1));
+    }
+    //////////////
+    return true;
+}
+
+function MascaraGenerica(seletor, tipoMascara) {
+    setTimeout(function () {
+        if (tipoMascara == 'CPF') {
+            formataCampo(seletor, '000.000.000-00');
+        } else if (tipoMascara == 'DATA') {
+            formataCampo(seletor, '00/00/0000');
+        } else if (tipoMascara == 'CEP') {
+            formataCampo(seletor, '00.000-000');
+        } else if (tipoMascara == 'TELEFONE') {
+            if (seletor.value.length <= 14) {
+                formataCampo(seletor, '(00) 0000-0000');
+            } else {
+                formataCampo(seletor, '(00) 00000-0000');
+            }
+
+        }
+    }, 200);
+}
+// Via CEP - início
+
+$(document).ready(function () {
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#rua").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+        $("#ibge").val("");
+    }
+
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(function () {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if (validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#rua").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+                $("#ibge").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#rua").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#uf").val(dados.uf);
+                        $("#ibge").val(dados.ibge);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
