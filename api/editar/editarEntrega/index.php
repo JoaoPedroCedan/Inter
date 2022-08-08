d288ff1c30ba8850d6435b56697418e8dbeff687
JoaoPedroCedan <joao.pedro@oaktecnologia.com>
2022-08-08 11:45:55 -0300

    aiad

---
+++ api/editar/editarEntrega/index.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once '../../conectiondb.php';
+
+$id = $_POST['id'];
+$is_active = $_POST['is_active'];
+
+$update = $conn->prepare("UPDATE entregas SET status = '$is_active' WHERE id = '$id'");
+$update->execute();
+echo json_encode('executado com sucesso');
+?>
